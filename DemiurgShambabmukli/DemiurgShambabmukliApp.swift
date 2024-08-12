fcbb2a219e8e27cc8210ccfebad58f35dfca52b5
Nikita Rylskiy <todoskatebaby@gmail.com>
2024-08-12 20:05:21 +0300

    Initial Commit

---
+++ DemiurgShambabmukli/DemiurgShambabmukliApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemiurgShambabmukliApp.swift
+//  DemiurgShambabmukli
+//
+//  Created by nikita on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemiurgShambabmukliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
